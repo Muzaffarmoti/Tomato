541fdd906e3782810e2d8cb48e3266f8841b3a6c
Aditya Kumar Mishra <akm998800hj@gmail.com>
2024-05-10 23:51:41 +0530

    timeline add

---
+++ src/Home.jsx
@@ -0,0 +1,54 @@
+
+import "./Home.css"
+const  Home= () => {
+  return (
+    <div>
+    <div className="timeline">
+    <div className="container left">
+      <div className="content">
+        <h2>2017</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div>
+    <div className="container right">
+      <div className="content">
+        <h2>2016</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div>
+    <div className="container left">
+      <div className="content">
+        <h2>2017</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div>
+    <div className="container right">
+      <div className="content">
+        <h2>2016</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div>
+    <div className="container left">
+      <div className="content">
+        <h2>2017</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div>
+    <div className="container right">
+      <div className="content">
+        <h2>2016</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div><div className="container left">
+      <div className="content">
+        <h2>2017</h2>
+        <p>Lorem ipsum..</p>
+      </div>
+    </div>
+    
+  </div>
+    </div>
+  )
+}
+
+export default Home
