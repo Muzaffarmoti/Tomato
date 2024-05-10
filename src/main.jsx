de9cd31114694dfcd038bdb827694a844d0f6bb0
Muzaffarmoti <muzaffarmoti@gmail.com>
2024-05-11 00:03:31 +0530

    changes in the main.jsx'

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import AppRoutes from './AppRoutes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AppRoutes />
+    </Router>
   </React.StrictMode>,
 )
